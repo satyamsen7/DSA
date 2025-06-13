e8d4420caa2b2526c822504f90209a2ea07e3d62
satyam sen <satyamsen267@gmail.com>
2025-06-14 02:03:48 +0530

    Add code: Print the Elements of a Linked List

---
+++ 14_06_2025_print_the_elements_of_a_linked_list.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+void printLinkedList(Node* head) {
+    Node* current = head;
+    while (current != nullptr) {
+        std::cout << current->data << " ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    Node* head = nullptr;
+    Node* second = nullptr;
+    Node* third = nullptr;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = nullptr;
+
+    printLinkedList(head);
+
+    delete head;
+    delete second;
+    delete third;
+
+    return 0;
+}

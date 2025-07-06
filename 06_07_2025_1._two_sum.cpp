1387c3f6a68bb2f0ab926d4cc8640617bcf30bd9
satyam sen <satyamsen267@gmail.com>
2025-07-06 15:51:48 +0530

    Add code: 1. Two Sum

---
+++ 06_07_2025_1._two_sum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> numMap;
+    for (int i = 0; i < nums.size(); ++i) {
+        int complement = target - nums[i];
+        if (numMap.find(complement) != numMap.end()) {
+            return {numMap[complement], i};
+        }
+        numMap[nums[i]] = i;
+    }
+    return {};
+}
+
+int main() {
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    std::vector<int> result = twoSum(nums, target);
+    if (!result.empty()) {
+        std::cout << result[0] << " " << result[1] << std::endl;
+    }
+    return 0;
+}

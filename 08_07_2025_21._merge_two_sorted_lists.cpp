54fa1c62e7b347f43ebe1f9e522822874d4c3e5e
satyam sen <satyamsen267@gmail.com>
2025-07-08 20:32:25 +0530

    Add code: 21. Merge Two Sorted Lists

---
+++ 08_07_2025_21._merge_two_sorted_lists.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* dummy = new ListNode();
+    ListNode* tail = dummy;
+
+    while (list1 != nullptr && list2 != nullptr) {
+        if (list1->val <= list2->val) {
+            tail->next = list1;
+            list1 = list1->next;
+        } else {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
+    }
+
+    if (list1 != nullptr) {
+        tail->next = list1;
+    } else {
+        tail->next = list2;
+    }
+
+    ListNode* head = dummy->next;
+    delete dummy;
+    return head;
+}

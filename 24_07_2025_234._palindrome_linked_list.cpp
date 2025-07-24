bf184fe94460bbf150d71fe2ad500e1363057b8e
satyam sen <satyamsen267@gmail.com>
2025-07-24 10:06:24 +0530

    Add code: 234. Palindrome Linked List

---
+++ 24_07_2025_234._palindrome_linked_list.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+bool isPalindrome(ListNode* head) {
+    vector<int> values;
+    ListNode* current = head;
+    while (current != nullptr) {
+        values.push_back(current->val);
+        current = current->next;
+    }
+
+    int left = 0;
+    int right = values.size() - 1;
+
+    while (left < right) {
+        if (values[left] != values[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
